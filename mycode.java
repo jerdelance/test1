5ff215e14d5727edc8c16e4f96513dd09c5c8828
Fabian Hallo <jerdelance@gmail.com>
2021-11-04 19:14:10 -0500

    add username and passworkd

--- mycode.java
+++ mycode.java
@@ -1,6 +1,7 @@
 public class mycode
 {
 	public static void main(String[] args) {
+		string usr = "***REMOVED***"
 		string pass = "***REMOVED***";
 		System.out.println(pass);
 		System.out.println("Hello World!");
